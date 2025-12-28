f3bb3b6faae234cae1e5e9c0953a47c86c98fc18
DViko <mymental.darkness@gmail.com>
2025-12-28 20:12:02 +0100

    fix basic_level/homework_08_02

--- basic_level/homework_08/02/main.cpp
+++ basic_level/homework_08/02/main.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 #include <exception>
+#include <array>
 #include <memory>
 #include <format>
 
-class ShapeError final : public std::domain_error
+class TriangularLogicError : public std::logic_error
 {
     public:
 
-        explicit ShapeError(const std::string& message) : std::domain_error(message)
+        explicit TriangularLogicError(const std::string& message) : std::logic_error(message)
         {
         }
 };
 
-class IShape
+class TriangularDomainError final : public TriangularLogicError
 {
     public:
 
-        virtual ~IShape() = default;
+        using TriangularLogicError::TriangularLogicError;
+};
+
+class ITriangular
+{
+    public:
+
+        virtual ~ITriangular() = default;
         virtual void Description() const = 0;
 };
 
-class ATriangle : public IShape
+class ATriangular : public ITriangular
 {
     private:
 
@@ -28,22 +36,13 @@ class ATriangle : public IShape
         std::array<int, 3> _edges;
         std::array<int, 3> _angles;
 
-        bool _Check(int A, int B, int C)
-        {
-
-            return (A + B + C == 180);
-        }
-
     protected:
 
-        ATriangle(std::string name, int a, int b, int c, int A, int B, int C)
+        ATriangular(std::string name, int a, int b, int c, int A, int B, int C)
         {
-            if (!_Check(A, B, C))
+            if (a + b <= c || a + c <= b || b + c <= a)
             {
-                throw ShapeError
-                (
-                    "Shape " + name + ", cannot be created. Reason: The sum of the angles does not equal 180"
-                );
+                throw TriangularLogicError("Edges length must be positive and greater than zero.");
             }
 
             _name = name;
@@ -51,65 +50,129 @@ class ATriangle : public IShape
             _angles = {A, B, C};
         }
 
+        const std::array<int, 3>& Angles() const
+        {
+            return _angles;
+        }
+
     public:
 
         void Description() const override final
         {
-            std::cout << _name << ":\n";
             std::cout << std::format
             (
-                "Edges: a: {}, b: {}, c: {}; Angles: A: {}, B: {}, C: {}\n\n",
+                "{}: Edges: a: {}, b: {}, c: {}; Angles: A: {}, B: {}, C: {}\n",
+                _name,
                 _edges[0], _edges[1], _edges[2],
                 _angles[0], _angles[1], _angles[2]
             );
         }
 };
 
-class RightTriangle final : public ATriangle
+class RightTriangle final : public ATriangular
 {
     public:
 
-        RightTriangle() : ATriangle("Right triangle", 3, 4, 5, 30, 50, 90)
+        RightTriangle() : ATriangular("Right triangle", 3, 4, 5, 50, 50, 80)
         {
+            auto [A, B, C] = Angles();
+
+            if(A != 90 && B != 90 && C != 90)
+            {
+                throw TriangularDomainError("A right triangle must have one angle equal to 90 degrees.");
+            }
         }
 };
 
-class EquilateralTriangle final : public ATriangle
+class EquilateralTriangle final : public ATriangular
 {
     public:
 
-        EquilateralTriangle() : ATriangle("Equilateral triangle", 30, 30, 30, 60, 60, 60)
+        EquilateralTriangle() : ATriangular("Equilateral triangle", 30, 30, 30, 60, 60, 60)
         {
+            auto [A, B, C] = Angles();
+
+            if(A != 60 || B != 60 || C != 60)
+            {
+                throw TriangularDomainError("An equilateral triangle must have all angles equal to 60 degrees.");
+            }
         }
 };
 
-void PrintDescription(const IShape* shape)
+class Triangle final : public ATriangular
+{
+    public:
+
+        Triangle() : ATriangular("Triangle", 0, 5, 7, 50, 60, 70)
+        {
+            auto [A, B, C] = Angles();
+
+            if(A + B + C != 180)
+            {
+                throw TriangularDomainError("The sum of the angles in a triangle must be 180 degrees.");
+            }
+        }
+};
+
+class SomeTriangular final : public ATriangular
+{
+    public:
+
+        SomeTriangular() : ATriangular("Some triangular", 9, 5, 7, 50, 60, 70)
+        {
+            throw std::runtime_error("Simulated system error.");
+        }
+};
+
+template<typename ITriangular>
+std::unique_ptr<ITriangular> CreateTriangular()
+{
+    try
+    {
+        return std::make_unique<ITriangular>();
+    }
+    catch(const TriangularDomainError& error)
+    {
+        std::cerr << "[triangular domain error]: " << error.what() << '\n';
+    }
+    catch(const TriangularLogicError& error)
+    {
+        std::cerr << "[triangular logic error]: " << error.what() << '\n';
+    }
+    catch (const std::exception& error)
+    {
+        std::cerr << "[std error]: " << error.what() << '\n';
+    }
+
+    return std::unique_ptr<ITriangular>{};
+}
+
+void PrintDescription(const ITriangular* shape)
 {
     shape -> Description();
 }
 
-
 int main ()
 {
-    std::unique_ptr<IShape> shape {};
-
-    try
+    if (auto shape = CreateTriangular<Triangle>())
     {
-        PrintDescription(std::make_unique<EquilateralTriangle>().get());
-    }
-    catch(const ShapeError& error)
-    {
-        std::cerr << "[shape error]: " << error.what() << "\n\n";
+        PrintDescription(shape.get());
     }
 
-    try
+    if (auto shape = CreateTriangular<EquilateralTriangle>())
     {
-        PrintDescription(std::make_unique<RightTriangle>().get());
+        PrintDescription(shape.get());
     }
-    catch(const ShapeError& error)
+
+    if (auto shape = CreateTriangular<RightTriangle>())
     {
-        std::cerr << "[shape error]: " << error.what() << "\n\n";
+        PrintDescription(shape.get());
     }
-    
+
+    if (auto shape = CreateTriangular<SomeTriangular>())
+    {
+        PrintDescription(shape.get());
+    }
+
     return EXIT_SUCCESS;
 }
